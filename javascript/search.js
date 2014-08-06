6c6601dd8cf8501bb374c43936e3d5e2dc61e009
marcoaam <marco.army@gmail.com>
2014-08-06 21:00:37 +0100

    search by city

--- javascript/search.js
+++ javascript/search.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 	$('#search_form').on('submit', function(event) {
 		event.preventDefault();
-		var url = "https://api.foursquare.com/v2/venues/search?near=" + $("#location").val() + ",London,Uk&client_id=VQZAZ1KSE1ARUSEB14NOPUIIA0GOUZWQQCSVMCV4GITH0EHN&client_secret=FOXUGMDYG2EWRLZAT3A5MDXOWV1MUZLR1WRT4EOVVGYOYVNU&v=20140806";
+		var url = 'https://api.foursquare.com/v2/venues/search?near=' + $('#city:checked').val() + ',venezuela&query=' + $("#interest").val() + '&client_id=VQZAZ1KSE1ARUSEB14NOPUIIA0GOUZWQQCSVMCV4GITH0EHN&client_secret=FOXUGMDYG2EWRLZAT3A5MDXOWV1MUZLR1WRT4EOVVGYOYVNU&v=20140806';
 		$.get(url, function(search) {
+			$('#results').text('');
 			search.response.venues.forEach(function(restaurant) {
-				console.log(restaurant.name);
+				$('<div id="result">' + restaurant.name + '</div>').prependTo('#results');
 			});
 		});
 	});
