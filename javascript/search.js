9288d76b55560db1c9d88420273b54a593f04888
marcoaam <marco.army@gmail.com>
2014-08-07 18:17:30 +0100

    Working version without css

--- javascript/search.js
+++ javascript/search.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
+	var restaurantsLocations = []
+	var map = new GMaps({
+	    div: '#map',
+	    lat: 51.5138687,
+	    lng: -0.1129165
+	});
 	$('#search_form').on('submit', function(event) {
 		event.preventDefault();
 		var url = 'https://api.foursquare.com/v2/venues/search?near=' + $('#city:checked').val() + ',London,Uk&query=' + $("#interest").val() + '&client_id=VQZAZ1KSE1ARUSEB14NOPUIIA0GOUZWQQCSVMCV4GITH0EHN&client_secret=FOXUGMDYG2EWRLZAT3A5MDXOWV1MUZLR1WRT4EOVVGYOYVNU&v=20140806';
 		$.get(url, function(search) {
 			$('#results').text('');
+			map.removeMarkers();
 			search.response.venues.forEach(function(restaurant) {
 				$('<div id="result">' + restaurant.name + '<br>' + restaurant.location.formattedAddress[0] + '</div>').prependTo('#results');
+				console.log(restaurant.location);
+				map.setCenter(51.5138687,-0.1129165);
+				map.setZoom(12);
+				map.addMarker({
+				  lat: restaurant.location.lat,
+				  lng: restaurant.location.lng,
+				  infoWindow: {
+             content: restaurant.name
+          }
+				});
 			});
 		});
 	});
