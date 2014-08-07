94baa5938ad20fbd6c44b0c7e14d2a3ade6a24d1
marcoaam <marco.army@gmail.com>
2014-08-07 19:01:31 +0100

    Map zoomed out

--- javascript/search.js
+++ javascript/search.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
 	var map = new GMaps({
 	    div: '#map',
 	    lat: 51.5138687,
-	    lng: -0.1129165
+	    lng: -0.1129165,
+	    zoom: 12
 	});
 	$('#search_form').on('submit', function(event) {
 		event.preventDefault();
-		var url = 'https://api.foursquare.com/v2/venues/search?near=' + $('#city:checked').val() + ',London,Uk&query=' + $("#interest").val() + '&client_id=VQZAZ1KSE1ARUSEB14NOPUIIA0GOUZWQQCSVMCV4GITH0EHN&client_secret=FOXUGMDYG2EWRLZAT3A5MDXOWV1MUZLR1WRT4EOVVGYOYVNU&v=20140806';
+		var url = 'https://api.foursquare.com/v2/venues/search?near=' + $('#city:checked').val() + ',London,Uk&query=' + $("#interest").val() + '&radius=1000&client_id=VQZAZ1KSE1ARUSEB14NOPUIIA0GOUZWQQCSVMCV4GITH0EHN&client_secret=FOXUGMDYG2EWRLZAT3A5MDXOWV1MUZLR1WRT4EOVVGYOYVNU&v=20140806';
 		$.get(url, function(search) {
 			$('#results').text('');
 			map.removeMarkers();
