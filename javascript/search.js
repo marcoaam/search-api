15103f09de6c7a350df1e6c6c5c9cd528ad905d4
marcoaam <marco.army@gmail.com>
2014-08-07 14:09:52 +0100

    Address added

--- javascript/search.js
+++ javascript/search.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$('#search_form').on('submit', function(event) {
 		event.preventDefault();
-		var url = 'https://api.foursquare.com/v2/venues/search?near=' + $('#city:checked').val() + ',venezuela&query=' + $("#interest").val() + '&client_id=VQZAZ1KSE1ARUSEB14NOPUIIA0GOUZWQQCSVMCV4GITH0EHN&client_secret=FOXUGMDYG2EWRLZAT3A5MDXOWV1MUZLR1WRT4EOVVGYOYVNU&v=20140806';
+		var url = 'https://api.foursquare.com/v2/venues/search?near=' + $('#city:checked').val() + ',London,Uk&query=' + $("#interest").val() + '&client_id=VQZAZ1KSE1ARUSEB14NOPUIIA0GOUZWQQCSVMCV4GITH0EHN&client_secret=FOXUGMDYG2EWRLZAT3A5MDXOWV1MUZLR1WRT4EOVVGYOYVNU&v=20140806';
 		$.get(url, function(search) {
 			$('#results').text('');
 			search.response.venues.forEach(function(restaurant) {
-				$('<div id="result">' + restaurant.name + '</div>').prependTo('#results');
+				$('<div id="result">' + restaurant.name + '<br>' + restaurant.location.formattedAddress[0] + '</div>').prependTo('#results');
 			});
 		});
 	});
